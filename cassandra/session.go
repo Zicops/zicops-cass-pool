279b196976fe8e9a64fae7650dc6b2e0544587c6
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-09-14 10:53:46 -0500

    improve connection

--- cassandra/session.go
+++ cassandra/session.go
@@ -2,14 +2,19 @@ package cassandra
 
 import gocqlx "github.com/scylladb/gocqlx/v2"
 
+var GlobalSession *gocqlx.Session
+
 func GetCassSession(keyspace string) (*gocqlx.Session, error) {
-	cluster, err := New(NewCassandraConfig(keyspace))
-	if err != nil {
-		return nil, err
+	if GlobalSession == nil || GlobalSession.Closed() {
+		cluster, err := New(NewCassandraConfig(keyspace))
+		if err != nil {
+			return nil, err
+		}
+		session, err := gocqlx.WrapSession(cluster.CreateSession())
+		if err != nil {
+			return nil, err
+		}
+		GlobalSession = &session
 	}
-	session, err := gocqlx.WrapSession(cluster.CreateSession())
-	if err != nil {
-		return nil, err
-	}
-	return &session, nil
+	return GlobalSession, nil
 }
